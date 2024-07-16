f53247b5701d7bbc5b2e7e8071a459ef25f27111
342himanshu <342himanshu@gmail.com>
2024-07-17 02:20:34 +0530

    adding modules and component

---
+++ src/app/reservation/reservation.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
+
+@NgModule({
+  declarations: [ReservationFormComponent],
+  imports: [CommonModule],
+})
+export class ReservationModule {}
